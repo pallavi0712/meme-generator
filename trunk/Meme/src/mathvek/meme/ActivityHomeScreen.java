e9d233434f4b87dae283c5d6889c2af68938690d
dhiren@veks.co.uk <dhiren@veks.co.uk@cfa69752-7216-a4cd-2b26-62f86fbdde93>
2012-03-20 17:35:46 +0000

    Initial Commit

---
+++ trunk/Meme/src/mathvek/meme/ActivityHomeScreen.java
@@ -0,0 +1,13 @@
+package mathvek.meme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ActivityHomeScreen extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
